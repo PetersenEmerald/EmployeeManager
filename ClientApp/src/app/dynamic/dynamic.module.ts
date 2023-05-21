090543479c0e1d69135979ac00b86cf4b447c855
Mae <velopias@gmail.com>
2023-05-21 14:33:35 -0500

    Adding scrollbar to dialog for reactive screen.

--- ClientApp/src/app/dynamic/dynamic.module.ts
+++ ClientApp/src/app/dynamic/dynamic.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -19,6 +20,7 @@ import { TableComponent } from './table/table.component';
     CommonModule, 
     FormsModule,
     MatButtonModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
