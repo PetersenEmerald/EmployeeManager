e197986d256252738cffc9a1b559046429af4fba
PetersenEmerald <petersen.emerald@gmail.com>
2023-05-29 22:17:22 -0500

    Removing unused app.module imports.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,21 +2,7 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSortModule } from '@angular/material/sort';
 import { MatTabsModule } from '@angular/material/tabs';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DynamicModule } from './dynamic/dynamic.module';
 import { RouterModule } from '@angular/router';
 
@@ -28,26 +14,12 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     HomeComponent
   ],
-  exports: [DynamicModule, MatSortModule],
+  exports: [DynamicModule],
   imports: [
-    BrowserAnimationsModule,
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     CommonModule,
     DynamicModule,
-    FormsModule,
     HttpClientModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatNativeDateModule,
-    MatSelectModule,
-    MatSlideToggleModule,
-    MatSortModule,
     MatTabsModule,
-    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
     ])
